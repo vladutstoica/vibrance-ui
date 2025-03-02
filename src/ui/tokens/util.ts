525fe016e1c73ca7efdfe589ef53fb196ffa902b
Vlad Stoica <vlad.stoica@vibrance.agency>
2025-03-03 00:02:02 +0200

    Refactor variable names for clarity in hslToHex function.
    Renamed variables to improve readability and better convey their purpose. These changes make the function easier to understand and maintain without altering its functionality.

--- src/ui/tokens/util.ts
+++ src/ui/tokens/util.ts
@@ -1,9 +1,9 @@
 export const hslToHex = (h: number, s: number, l: number) => {
-  l /= 100;
-  const a = (s * Math.min(l, 1 - l)) / 100;
+  const lightness = l / 100;
+  const a = (s * Math.min(lightness, 1 - lightness)) / 100;
   const f = (n: number) => {
     const k = (n + h / 30) % 12;
-    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    const color = lightness - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
     return Math.round(255 * color)
       .toString(16)
       .padStart(2, "0"); // convert to Hex and prefix "0" if needed
